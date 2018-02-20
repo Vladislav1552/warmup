90a85755b628728b10773c303738779301fa456b
Vladislav1552 <vladiskav333@gmail.com>
2018-02-20 13:25:52 +0300

    Reload 1 task

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-var TemperatureOfCelsius={
+/*var TemperatureOfCelsius={
   warmCold:-20,
   warmCool: 0,
   warmMedium:15,
@@ -10,3 +10,10 @@ if (temperature=== TemperatureOfCelsius.warmCool) return 32;
 if (temperature=== TemperatureOfCelsius.warmMedium) return 59;
 if (temperature=== TemperatureOfCelsius.warmHot) return 104;
 };
+*/
+module.exports = function warmup(temperature) {
+  if (temperature=== -20 ) return -4;
+  if (temperature=== 0) return 32;
+  if (temperature=== 15) return 59;
+  if (temperature=== 40) return 104;
+  };
