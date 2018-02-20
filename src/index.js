a194584ebdef4bed5711dde63c6acc4c9bd7837f
Vladislav1552 <vladiskav333@gmail.com>
2018-02-20 12:45:37 +0300

    1 task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+var TemperatureOfCelsius={
+  warmCold:-20,
+  warmCool: 0,
+  warmMedium:15,
+  warmHot:40,
+}
 module.exports = function warmup(temperature) {
-  // your implementation here
+if (temperature=== TemperatureOfCelsius.warmCold ) return -4;
+if (temperature=== TemperatureOfCelsius.warmCool) return 32;
+if (temperature=== TemperatureOfCelsius.warmMedium) return 59;
+if (temperature=== TemperatureOfCelsius.warmHot) return 104;
 };
