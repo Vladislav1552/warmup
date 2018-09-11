70a9d877cab8532cf45239b67cacd48cfde0a107
Vladislav1552 <vladiskav333@gmail.com>
2018-09-11 23:02:52 +0300

    1st task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
-var TemperatureOfCelsius={
-  warmCold:-20,
-  warmCool: 0,
-  warmMedium:15,
-  warmHot:40,
-}
+
 module.exports = function warmup(temperature) {
-if (temperature=== TemperatureOfCelsius.warmCold ) return -4;
-if (temperature=== TemperatureOfCelsius.warmCool) return 32;
-if (temperature=== TemperatureOfCelsius.warmMedium) return 59;
-if (temperature=== TemperatureOfCelsius.warmHot) return 104;
+return temperature*1.8+32;
 };
